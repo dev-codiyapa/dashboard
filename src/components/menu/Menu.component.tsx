686e20575b94aef6ef7fd8de718440c732b8dd3f
AJMAC1032 <ajmac1032@AJMAC1098s-MacBook-Pro.local>
2020-06-21 03:55:45 +0530

    changes in menu

--- src/components/menu/Menu.component.tsx
+++ src/components/menu/Menu.component.tsx
@@ -8,6 +8,7 @@ import {
 import "./Menu.component.css";
 
 const { Sider } = Layout;
+const { SubMenu } = Menu;
 
 class SieBarMenu extends React.Component<any> {
   constructor(props: any) {
@@ -20,15 +21,24 @@ class SieBarMenu extends React.Component<any> {
       <Sider trigger={null} collapsible collapsed={this.props.collapsed}>
         <div className="logo" />
         <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            nav 1
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            nav 2
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            nav 3
-          </Menu.Item>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 1">
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 2">
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
+          <SubMenu key="sub1" icon={<UserOutlined />} title="subnav 3">
+            <Menu.Item key="1">option1</Menu.Item>
+            <Menu.Item key="2">option2</Menu.Item>
+            <Menu.Item key="3">option3</Menu.Item>
+            <Menu.Item key="4">option4</Menu.Item>
+          </SubMenu>
         </Menu>
       </Sider>
     );
