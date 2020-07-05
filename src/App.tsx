6533129a0475146219dd34856f70b33c0172d14d
AJMAC1032 <ajmac1032@AJMAC1098s-MacBook-Pro.local>
2020-07-05 20:32:23 +0530

    routing issue fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import Home from "./pages/home/Home.component";
 import "antd/dist/antd.css";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import CovidDetailsReport from "./pages/details/Details.component";
 
-function App() {
+const App: React.FC = () => {
   return (
-      <Router>
-        <Switch>
-          <Route path="/" component={Home} exact={true} />
-          <Route
-            path="/detailed_view"
-            component={CovidDetailsReport}
-            exact={false}
-          />
-        </Switch>
-      </Router>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/detailed_view">
+          <CovidDetailsReport />
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
