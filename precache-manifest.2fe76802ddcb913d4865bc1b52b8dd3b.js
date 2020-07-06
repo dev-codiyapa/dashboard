f00a3a46b8adde55cf093356ace0087aa15269f1
AJMAC1032 <ajmac1032@AJMAC1098s-MacBook-Pro.local>
2020-07-06 14:23:48 +0530

    Updates

---
+++ precache-manifest.2fe76802ddcb913d4865bc1b52b8dd3b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bdc52499bba5a152a3988e25b701015",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7697989f34dbcc54ec72",
+    "url": "./static/css/2.a2026422.chunk.css"
+  },
+  {
+    "revision": "cac267132a219b96a91d",
+    "url": "./static/css/main.e59cf10c.chunk.css"
+  },
+  {
+    "revision": "7697989f34dbcc54ec72",
+    "url": "./static/js/2.0d589815.chunk.js"
+  },
+  {
+    "revision": "ca3cfcda77dc9f4f71fda9fdfcf14fb0",
+    "url": "./static/js/2.0d589815.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cac267132a219b96a91d",
+    "url": "./static/js/main.17528f9b.chunk.js"
+  },
+  {
+    "revision": "e39353a74a755fe2f435",
+    "url": "./static/js/runtime-main.46185c08.js"
+  },
+  {
+    "revision": "d51ec02ec1a8cc98e4880abbe2d9570d",
+    "url": "./static/media/india.d51ec02e.png"
+  },
+  {
+    "revision": "4d3a07a1f46c1da6bb4363d8c8c0ed96",
+    "url": "./static/media/virus.4d3a07a1.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dashboard/precache-manifest.e7c02259e606bf5383bea5c6bf44470a.js"
+  "./precache-manifest.2fe76802ddcb913d4865bc1b52b8dd3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dashboard/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
